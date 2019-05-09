0b99896324b4677f58fa7d504189983abd746934
nicolas <nicolas.gutierrez.p@usach.cl>
2019-05-08 23:26:30 -0400

    Feat: se agregan cabeceras de funciones tiviales

---
+++ include/iofuctions.h
@@ -0,0 +1,9 @@
+#ifndef IOFUCTIONS_H
+    
+    #define IOFUCTIONS_H
+    
+    void readFile(char *file);
+    int countLines(char *file);
+    void getArgs(int argc, char *argv[], int *n_disk, int* n_whgt, char* in_file, char* out_file, int *flag);
+
+#endif
